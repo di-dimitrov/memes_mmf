f4f5912161caf1af2be6b27a1ffa9212e9d757e9
sash <sash@fb.com>
2020-11-11 11:41:24 -0800

    train_vqa_from_scratch

--- tools/sweeps/sweep_visual_bert.py
+++ tools/sweeps/sweep_visual_bert.py
@@ -7,27 +7,27 @@ from lib import hyperparam
 
 
 def get_grid(args):
-    max_update = 22000
+    max_update = 200_000
 
     return [
-        hyperparam("run_type", "train_val"),
-        hyperparam("config", "projects/visual_bert/configs/vqa2/defaults.yaml"),
+        hyperparam("run_type", "test"),
+        hyperparam("config", "projects/mmf_transformer/configs/vqa2/defaults.yaml"),
         # hyperparam("--fp16", save_dir_key=lambda val: "fp16"),
-        hyperparam("training.num_workers", 5),
+        hyperparam("training.num_workers", 0),
         hyperparam("dataset", "vqa2"),
-        hyperparam("model", "visual_bert", save_dir_key=lambda val: val),
+        hyperparam("model", "mmf_transformer", save_dir_key=lambda val: val),
         # For nlvr2, we are able to fit batch of size 16 on single GPU with 16GB
         # memory. Same number is 32 for VQA2, so scale accordingly
         hyperparam(
-            "training.batch_size", [512, 256], save_dir_key=lambda val: f"bs{val}"
+            "training.batch_size", [64], save_dir_key=lambda val: f"bs{val}"
         ),
-        hyperparam("training.seed", 1, save_dir_key=lambda val: f"s{val}"),
+        hyperparam("training.seed", -1, save_dir_key=lambda val: f"s{val}"),
         hyperparam("scheduler.type", ["warmup_cosine"]),
         hyperparam("scheduler.params.num_warmup_steps", 2000),
         hyperparam("scheduler.params.num_training_steps", max_update),
         hyperparam("optimizer.type", "adam_w", save_dir_key=lambda val: val),
         hyperparam(
-            "optimizer.params.lr", [5e-5, 1e-5], save_dir_key=lambda val: f"lr{val}"
+            "optimizer.params.lr", [5e-5], save_dir_key=lambda val: f"lr{val}"
         ),
         hyperparam("optimizer.params.eps", 1e-8),
         hyperparam(
@@ -37,13 +37,11 @@ def get_grid(args):
         hyperparam("training.pin_memory", True),
         hyperparam("training.log_interval", 1000),
         hyperparam("training.checkpoint_interval", 1000),
-        hyperparam("training.evaluation_interval", 4000),
+        hyperparam("training.evaluation_interval", 2000),
         hyperparam("training.find_unused_parameters", True),
-        hyperparam(
-            "model_config.visual_bert.freeze_base",
-            [False],
-            save_dir_key=lambda val: f"fb{val}",
-        ),
+        hyperparam("checkpoint.max_to_keep", 0),
+        hyperparam("checkpoint.resume_file", "/checkpoint/sash/jobs/mmf/checkpoints/mmf_transformer/11_10_2020_vqa2_from_scratch/vqa_scratch_bs64.mmf_transformer.bs64.s-1.adam_w.lr5e-05.mu200000.ngpu8/best.ckpt"),
+        hyperparam("evaluation.predict", True)
     ]
 
 
